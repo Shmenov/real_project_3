9f2f2254506608736e38d1dacdd21ed39d0f42d9
Shmenov <danchik_2005s@mail.ru>
2026-05-20 12:04:26 +0500

    feat: adaptation for phones

---
+++ js/index.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.getElementById('burger');
+  const menu = document.getElementById('menu');
+  const body = document.body;
+	const html = document.documentElement; // Получаем тег <html>
+
+  burger.addEventListener('click', () => {
+    // Переключаем классы
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-open');
+    
+    // Блокируем скролл сайта под открытым меню
+    body.classList.toggle('no-scroll');
+    html.classList.toggle('no-scroll');
+  });
+
+  // Закрываем меню при клике на любую ссылку внутри него (полезно для якорей)
+  const links = document.querySelectorAll('.header__link');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      burger.classList.remove('is-active');
+      menu.classList.remove('is-open');
+      body.classList.remove('no-scroll');
+      html.classList.remove('no-scroll');
+    });
+  });
+});
+
+/Убрать подсказку при свайпе на секции с карточками/
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Находим контейнер, который скроллится, и заголовок с подсказкой
+  const scrollContainer = document.querySelector('.products__cards');
+  const titleWithHint = document.querySelector('.card__image-wrapper--razor');
+
+  // Проверяем, существуют ли эти элементы на странице
+  if (scrollContainer && titleWithHint) {
+    
+    // Создаем функцию, которая будет проверять скролл
+    const hideHintOnScroll = () => {
+      // scrollLeft показывает, на сколько пикселей элемент прокручен влево.
+      // Если больше 10 пикселей (защита от случайных микро-касаний), прячем текст!
+      if (scrollContainer.scrollLeft > 10) {
+        titleWithHint.classList.add('is-hidden');
+        
+        // ВАЖНО: Удаляем слушатель событий! 
+        // Подсказка уже спрятана, нам больше не нужно следить за каждым миллиметром скролла.
+        scrollContainer.removeEventListener('scroll', hideHintOnScroll);
+      }
+    };
+
+    // Вешаем слушатель события 'scroll' на контейнер с карточками
+    scrollContainer.addEventListener('scroll', hideHintOnScroll);
+  }
+});
